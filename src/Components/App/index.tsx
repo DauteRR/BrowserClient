c0b4783a4301fb3e9c67a0485c57da86b923120e
Daute Rodríguez Rodríguez <alu0100973914@ull.edu.es>
2020-01-29 17:00:45 +0000

    Add: material-ui

--- src/Components/App/index.tsx
+++ src/Components/App/index.tsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { Typography, Button } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    app: {
+      width: '100vw',
+      height: '100vh',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }
+  })
+);
 
 const App: React.FC = () => {
-  return <div className='App'></div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.app}>
+      <Typography variant='h1'>Browser</Typography>
+      <Button variant='outlined'>Navigate</Button>
+    </div>
+  );
 };
 
 export default App;
